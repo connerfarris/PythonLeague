1de4f3b5578f016cd383f96ab26fefc1de8dfd51
cfarris <cfarris@clearwateranalytics.com>
2020-02-07 12:55:32 -0700

    lunch work

--- TestAPIRequest.py
+++ TestAPIRequest.py
@@ -1,23 +1,54 @@
 import requests
 
 riot_api_key = 'RGAPI-62da6753-373c-4e3f-b7ea-9117d8b21b39'
-riot_champInfo_url = 'https://na1.api.riotgames.com/lol/platform/v3/champion-rotations'
-riot_champInfo_request_url = riot_champInfo_url + '?' + riot_api_key
-riot_champInfo_request_headers = {
-    "Origin": "https://developer.riotgames.com",
-    "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-    "X-Riot-Token": "RGAPI-62da6753-373c-4e3f-b7ea-9117d8b21b39",
-    "Accept-Language": "en-US,en;q=0.5",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0"
-}
-response = requests.get(url=riot_champInfo_request_url, headers=riot_champInfo_request_headers)
-if response.status_code != 200:
-	raise Exception('Bad response code {}'.format(str(response.status_code)))
-json_response = response.json()
-print(json_response)
-freeChampionIds = json_response['freeChampionIds']
-print(freeChampionIds)
-freeChampionIdsForNewPlayers = json_response['freeChampionIdsForNewPlayers']
-print(freeChampionIdsForNewPlayers)
-maxNewPlayerLevel = json_response['maxNewPlayerLevel']
-print(maxNewPlayerLevel)
+
+def requestChampInfo():
+    url = 'https://na1.api.riotgames.com/lol/platform/v3/champion-rotations' + '?' + riot_api_key
+    headers = {
+        "Origin": "https://developer.riotgames.com",
+        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+        "X-Riot-Token": "RGAPI-62da6753-373c-4e3f-b7ea-9117d8b21b39",
+        "Accept-Language": "en-US,en;q=0.5",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0"
+    }
+    response = requests.get(url=url, headers=headers)
+    if response.status_code != 200:
+        raise Exception('Bad response code {}'.format(str(response.status_code)))
+    return response.json()
+
+def requestSummonerByName(summonerName):
+    summonerNameNoSpaces = summonerName.replace(' ', '%20')
+    url = 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerNameNoSpaces + '?' + riot_api_key
+    headers = {
+        "Origin": "https://developer.riotgames.com",
+        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+        "X-Riot-Token": "RGAPI-62da6753-373c-4e3f-b7ea-9117d8b21b39",
+        "Accept-Language": "en-US,en;q=0.5",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0"
+    }
+    response = requests.get(url=url, headers=headers)
+    if response.status_code != 200:
+        raise Exception('Bad response code {}'.format(str(response.status_code)))
+    return response.json()
+
+def requestLeagueEntriesByEncryptedSummonerId(encryptedSummonerId):
+    url = 'https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/' + encryptedSummonerId + '?api_key=' + riot_api_key
+    headers = {
+        "Origin": "https://developer.riotgames.com",
+        "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+        "X-Riot-Token": "RGAPI-62da6753-373c-4e3f-b7ea-9117d8b21b39",
+        "Accept-Language": "en-US,en;q=0.5",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0"
+    }
+    response = requests.get(url=url, headers=headers)
+    if response.status_code != 200:
+        raise Exception('Bad response code {}'.format(str(response.status_code)))
+    return response.json()
+
+
+print('free champ rotation: ')
+print(requestChampInfo())
+print('summoner account info: ')
+print(requestSummonerByName('C9 Jhin Erso'))
+print('summoner league stats: ')
+print(requestLeagueEntriesByEncryptedSummonerId(requestSummonerByName('C9 Jhin Erso')['id']))
